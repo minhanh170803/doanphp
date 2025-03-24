9cd9a79444b64c47ee0d8155ae640e52883fd7b7
ma <trinhminhanh170803@gmail.com>
2025-03-24 10:30:40 +0700

    create project + chuc nang

---
+++ DB_connection.php
@@ -0,0 +1,16 @@
+<?php
+$sName = "127.0.0.1";  // Địa chỉ máy chủ MySQL
+$uName = "root";       // Tên đăng nhập MySQL
+$pass  = "1234";       // Mật khẩu MySQL
+$db_name = "phpdb";    // Tên cơ sở dữ liệu
+$port = "3307";        // Cổng MySQL (3307 theo yêu cầu)
+
+
+
+try {
+	$conn = new PDO("mysql:host=$sName;dbname=$db_name;port=$port", $uName, $pass);
+	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+	echo "Connection failed: " . $e->getMessage();
+	exit;
+}
